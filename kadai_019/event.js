fab766edeb3cf7eb5ce0c986d980605e0cc1ecdd
nattougohann <138192429+nattougohann@users.noreply.github.com>
2023-08-03 21:51:15 +0900

    Add files via upload

---
+++ kadai_019/event.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("btn");
+const text1 = document.getElementById('text');
+
+
+btn.addEventListener("click", ()=> {
+  const text2 = document.createElement("h2");
+  text1.textContent = "ボタンをクリックしました";
+});
